8dee9d3811a902eb08bee6c0d0d302b0e362681e
Olivier Cuisenaire <olivier.cuisenaire@heig-vd.ch>
2023-03-14 13:43:32 +0100

    Solution enoncé 13 (hi-res normals in texture image)

--- 13 - Normal texture/NormalTextureShader.h
+++ 13 - Normal texture/NormalTextureShader.h
@@ -1,14 +1,10 @@
 #ifndef CPPRENDERER_NORMALTEXTURESHADER_H
 #define CPPRENDERER_NORMALTEXTURESHADER_H
 
-#include "Shader.h"
+#include "GouraudTextureShader.h"
 
-struct NormalTextureShader :
-        public Shader,
-        public TextureShaderBase
-        {
+struct NormalTextureShader : public GouraudTextureShader {
    TGAImage* normals;
-   virtual Vec3f vertex(size_t face, size_t vert);
    virtual bool fragment(Vec3f bary, TGAColor& color);
 };
 
