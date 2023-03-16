5b4e0cd0d04a90729774be4723cf7f50cedbbf9f
Olivier Cuisenaire <olivier.cuisenaire@heig-vd.ch>
2023-03-16 14:23:17 +0100

    cleaner inheritance for NormalTextureShader

--- 13 - Normal texture/NormalTextureShader.cpp
+++ 13 - Normal texture/NormalTextureShader.cpp
@@ -1,12 +1,17 @@
 #include "NormalTextureShader.h"
 
+Vec3f NormalTextureShader::vertex(size_t face, size_t vert) {
+   TextureShaderBase::vertex(model, face, vert);
+   return Shader::vertex(face,vert);
+}
+
 bool NormalTextureShader::fragment(Vec3f bary, TGAColor &color) {
    TextureShaderBase::fragment(bary, color);
    Vec3f ptc = varying_uv[0] * bary.x + varying_uv[1] * bary.y + varying_uv[2] * bary.z;
    Vec2i pc(int(round(ptc.x * float(normals->get_width()))),int(round(ptc.y * float(normals->get_height()))));
    TGAColor normal = normals->get(pc.x, pc.y);
-   auto n = Vec3f{ float(normal.raw[2]), float(normal.raw[1]), float(normal.raw[0]) } * (2.f/255.f)
-           - Vec3f{1.f,1.f,1.f};
+   auto n = Vec3f{ float(normal.raw[2]), float(normal.raw[1]), float(normal.raw[0]) }
+            - Vec3f{127.5f,127.5f,127.5f};
    n.normalize();
 
    float pixelIntensity = n * light;
--- 13 - Normal texture/NormalTextureShader.h
+++ 13 - Normal texture/NormalTextureShader.h
@@ -1,10 +1,11 @@
 #ifndef CPPRENDERER_NORMALTEXTURESHADER_H
 #define CPPRENDERER_NORMALTEXTURESHADER_H
 
-#include "GouraudTextureShader.h"
+#include "FlatTextureShader.h"
 
-struct NormalTextureShader : public GouraudTextureShader {
+struct NormalTextureShader : public Shader, TextureShaderBase {
    TGAImage* normals;
+   virtual Vec3f vertex(size_t face, size_t vert);
    virtual bool fragment(Vec3f bary, TGAColor& color);
 };
 
